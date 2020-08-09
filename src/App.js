6085a7cc146b2b075f255e3761941e4b80059391
Jason <jason8maharjan@gmail.com>
2020-08-09 10:31:37 +0545

    initialized Ethereum blockchain with metamask

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
+import Web3 from 'web3';
 
 import Header from './components/header/header.component';
 import Home from './pages/home/home.component';
@@ -10,22 +11,43 @@ import Login from './pages/login/login.component';
 import Upload from './pages/upload/upload.component';
 import Footer from './components/footer/footer.component';
 
-function App() {
-  return (
-    <div className="App">
-      < Header />
-      <div className = "App-body">
-        <Switch>
-          <Route exact path = "/" component = {Home} />
-          <Route path = "/about" component = {About} />
-          <Route path = "/verify" component = {Verify} />
-          <Route path = "/login" component = {Login} />
-          <Route path = "/upload" component = {Upload} />
-        </Switch>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      account: ''
+    }
+  }
+
+  componentDidMount() {
+    this.loadBlockChainData();
+  }
+
+  async loadBlockChainData() {
+    const web3 = new Web3(Web3.givenProvider); // Provider from Metamask
+    const accounts = await web3.eth.getAccounts(); // Get Ethereum blockchain account
+
+    this.setState({account: accounts[0]});
+    console.log(accounts[0]);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        < Header />
+        <div className = "App-body">
+          <Switch>
+            <Route exact path = "/" component = {Home} />
+            <Route path = "/about" component = {About} />
+            <Route path = "/verify" component = {Verify} />
+            <Route path = "/login" component = {Login} />
+            <Route path = "/upload" component = {Upload} />
+          </Switch>
+        </div>
+        < Footer />
       </div>
-      < Footer />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
